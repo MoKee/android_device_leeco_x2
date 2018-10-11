31b37cee1ca5cbe8c5cbc7f23d602a62b1be3452
Martincz Gao <martincz@mokeedev.com>
2018-10-11 17:36:47 +0800

    x2: mokee-ify
    Change-Id: I53cb6569a1b021148822198563964f408fc85d90

--- devicesettings/Android.mk
+++ devicesettings/Android.mk
@@ -21,7 +21,7 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
 
 ifeq ($(BOARD_AOSP_BASED),)
     LOCAL_STATIC_JAVA_LIBRARIES += \
-        org.lineageos.platform.internal
+        org.mokee.platform.internal
 endif
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
